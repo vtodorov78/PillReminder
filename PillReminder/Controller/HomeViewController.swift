76e38729d225b0d5a2adb7b285ca0e1471b0b2dd
vtodorov78 <vvtodorov12@gmail.com>
2023-08-16 00:02:59 +0300

    Initial commit

---
+++ PillReminder/Controller/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PillReminder
+//
+//  Created by Vladimir Todorov on 15.08.23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .systemBlue
+    }
+
+
+}
+
