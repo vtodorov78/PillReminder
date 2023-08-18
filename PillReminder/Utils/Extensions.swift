abcf6a837ad2630ac2ffdd21439bbb9200784c54
vtodorov78 <vvtodorov12@gmail.com>
2023-08-18 23:10:35 +0300

    add custom color and contraints extensions

---
+++ PillReminder/Utils/Extensions.swift
@@ -0,0 +1,56 @@
+//
+//  Extensions.swift
+//  PillReminder
+//
+//  Created by Vladimir Todorov on 18.08.23.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+    static func mainBlue() -> UIColor {
+        return UIColor.rgb(red: 0, green: 150, blue: 255)
+    }
+    
+    static func babyBlue() -> UIColor {
+        return UIColor.rgb(red: 183, green: 233, blue: 247)
+    }
+}
+
+extension UIView {
+    
+    func anchor(top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, right: NSLayoutXAxisAnchor?,  paddingTop: CGFloat, paddingLeft: CGFloat, paddingBottom: CGFloat, paddingRight: CGFloat, width: CGFloat, height: CGFloat) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let left = left {
+            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if width != 0 {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if height != 0 {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+}
